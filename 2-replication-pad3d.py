826925ed6b7f341c439153d213b3999c737efe93
Jokeren <robinho364@gmail.com>
2020-12-20 13:17:57 -0600

    Explicit use cuda

--- 2-replication-pad3d.py
+++ 2-replication-pad3d.py
@@ -5,7 +5,7 @@ import time
 dtype = torch.float
 device = torch.device("cuda:0")  # Uncomment this to run on GPU
 
-m = torch.nn.ReplicationPad3d(3)
+m = torch.nn.ReplicationPad3d(3).cuda()
 input = torch.randn((1, 16, 100, 100, 100), device=device, requires_grad=True)
 grad = torch.randn((1, 16, 106, 106, 106), device=device)
 output = torch.randn((1, 16, 106, 106, 106), device=device)
